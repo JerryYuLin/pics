35f09f2f14cdb8d1e16128c3d7295f3c153126b4
JerryYuLin <jerry19970319@gmail.com>
2020-12-21 22:16:42 +0800

    unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
     onSearchSubmit(term){
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos/', {
+            params: {
+                query: term
+            },
+            headers:{
+                Authorization: 'Client_ID bwaTKjAv6Xq43V9wlfeBckrjd5uki7lw5wD4PeBYApc'
+            }
+        });
     }
 
     render(){
